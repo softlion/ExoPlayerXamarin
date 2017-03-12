89ecf59469e7f82a19d4ed5bae609fd1c0165952
softlion <benjamin@vapolia.fr>
2017-03-12 11:55:15 +0100

    Add enum

---
+++ ExoPlayer/Additions/PlaybackState.cs
@@ -0,0 +1,20 @@
+using Android.Runtime;
+
+namespace Com.Google.Android.Exoplayer2
+{
+    /// <summary>
+    /// http://google.github.io/ExoPlayer/doc/reference-v1/com/google/android/exoplayer/ExoPlayer.html#State
+    /// </summary>
+    [Preserve]
+    public enum PlaybackState
+    {
+        //TrackDisabled = -1,
+        //UnknownTime = -1,
+        //TrackDefault = 0,
+        Idle = ExoPlayer.StateIdle,
+        //Preparing = 2,
+        Buffering = ExoPlayer.StateBuffering,
+        Ready = ExoPlayer.StateReady,
+        Ended = ExoPlayer.StateEnded,
+    }
+}
